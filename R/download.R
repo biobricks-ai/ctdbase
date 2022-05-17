e34c69f995bf7c0e56ba1d6bb847eb8b026f4d1d
Ajay Chatrath <achatrath12@gmail.com>
2022-05-17 10:51:51 -0500

    [ctdbase] Fixed changed URL in download.R

--- R/download.R
+++ R/download.R
@@ -5,8 +5,9 @@ library(fs)
 options(timeout=1800) # download timeout
 
 ctd  <- "https://ctdbase.org/downloads"
+address <- "https://ctdbase.org/"
 href <- read_html(ctd) |> html_elements("a") |> html_attr("href")
-tbls <- keep(href,~ grepl("(*.tsv|*.tsv.gz|*.obo)$",.)) |>  map_chr(~ paste0(ctd,.))
+tbls <- keep(href,~ grepl("(*.tsv|*.tsv.gz|*.obo)$",.)) |>  map_chr(~ paste0(address,.))
 
 outs <- fs::dir_create("download")
 file <- fs::path(outs,fs::path_file(tbls))
